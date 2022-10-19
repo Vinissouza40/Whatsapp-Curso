14dc7f1fb80d353f8eb5ddaff66676e01ef430f6
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-10-19 10:54:19 -0300

    NovaTela

--- app/src/main/java/com/example/whatsapp/LoginActivity.java
+++ app/src/main/java/com/example/whatsapp/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextInputEditText editEmail, editSenha;
+    private TextView textCadastro;
+    private Button buttonLogar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        buttonLogar = findViewById(R.id.buttonLogar);
+
+    }
+
+    public void abrirTelaCadastro(View view){
+      startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
     }
 }
