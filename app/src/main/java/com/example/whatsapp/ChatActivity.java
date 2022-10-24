0448f4bbfa7901ebd7f5623f9846e0c0753183f0
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-10-24 17:43:02 -0300

    Modificacoes

--- app/src/main/java/com/example/whatsapp/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/ChatActivity.java
@@ -4,15 +4,28 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+import com.example.whatsapp.adapter.MensagensAdapter;
+import com.example.whatsapp.helper.Base64Custom;
+import com.example.whatsapp.helper.UsuarioFirebase;
+import com.example.whatsapp.model.Mensagem;
 import com.example.whatsapp.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -21,6 +34,14 @@ public class ChatActivity extends AppCompatActivity {
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
     private Usuario usuarioDestinatario;
+    private EditText editMensagem;
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
+    private DatabaseReference msgDatabase = FirebaseDatabase.getInstance().getReference();
+    private RecyclerView recyclerMensagens;
+    private MensagensAdapter adapter;
+    private List<Mensagem> mensagens = new ArrayList<>();
+
 
     @Override
 
@@ -32,26 +53,68 @@ public class ChatActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        textViewNome  = findViewById(R.id.textViewnomeChat);
+        textViewNome = findViewById(R.id.textViewnomeChat);
         circleImageViewFoto = findViewById(R.id.circleImageViewFotoChat);
+        editMensagem = findViewById(R.id.editMensagem);
+        recyclerMensagens = findViewById(R.id.recyclerMensagens);
+
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
+
 
         Bundle bundle = getIntent().getExtras();
 
-        if (bundle != null){
+        if (bundle != null) {
             usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
             textViewNome.setText(usuarioDestinatario.getNome());
 
             String foto = usuarioDestinatario.getFoto();
 
-            if (foto != null){
+            if (foto != null) {
                 Uri uri = Uri.parse(usuarioDestinatario.getFoto());
                 Glide.with(ChatActivity.this).load(uri).into(circleImageViewFoto);
-            }else{
+            } else {
                 circleImageViewFoto.setImageResource(R.drawable.padrao);
             }
+
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
+
+        adapter = new MensagensAdapter(mensagens, getApplicationContext());
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMensagens.setLayoutManager(layoutManager);
+        recyclerMensagens.setHasFixedSize(true);
+        recyclerMensagens.setAdapter(adapter);
 
     }
 
+    public void enviarMensagem(View view) {
+        String textoMensagem = editMensagem.getText().toString();
 
+        if (!textoMensagem.isEmpty()) {
+
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+
+        } else {
+            Toast.makeText(ChatActivity.this, "Digite uma mensagem para enviar", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg) {
+
+        DatabaseReference mensagemRef = msgDatabase.child("mensagens");
+
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(msg);
+
+
+        editMensagem.setText("");
+
+    }
 }
