b3c6201db8d36a6d0409913ff5ba88f52cc02f5c
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-10-26 11:13:53 -0300

    ListaContatos

--- app/src/main/java/com/example/whatsapp/GrupoActivity.java
+++ app/src/main/java/com/example/whatsapp/GrupoActivity.java
@@ -2,15 +2,37 @@ package com.example.whatsapp;
 
 import android.os.Bundle;
 
+import com.example.whatsapp.adapter.ContatosAdapter;
+import com.example.whatsapp.helper.UsuarioFirebase;
+import com.example.whatsapp.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GrupoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerMembrosSelecionados, recyclerMembros;
+    private ContatosAdapter contatosAdapter;
+    private List<Usuario> listaMembros = new ArrayList<>();
+    private ValueEventListener valueEventListenerMembros;
+    private DatabaseReference usuarioRef;
+    private FirebaseUser usuarioAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +40,8 @@ public class GrupoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grupo);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +49,58 @@ public class GrupoActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        recyclerMembros = findViewById(R.id.recyclerMembros);
+        recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosSelecionados);
+
+        usuarioRef = FirebaseDatabase.getInstance().getReference().child("usuarios");
+        usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+
+        contatosAdapter = new ContatosAdapter(listaMembros, getApplicationContext());
+
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMembros.setLayoutManager(layoutManager);
+        recyclerMembros.setHasFixedSize(true);
+        recyclerMembros.setAdapter(contatosAdapter);
+        recuperarContatos();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuarioRef.removeEventListener(valueEventListenerMembros);
+    }
+
+    public void recuperarContatos() {
+        valueEventListenerMembros = usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dados : snapshot.getChildren()) {
+
+
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    String emailUsuarioAtual = usuarioAtual.getEmail();
+                    if (!emailUsuarioAtual.equals(usuario.getEmail())) {
+                        listaMembros.add(usuario);
+                    }
+                }
+                contatosAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
